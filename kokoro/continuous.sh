9b142160b5e8da9a62ec243b843cb059a38edd65
Appu Goundan <appu@google.com>
2017-07-31 09:25:14 -0400

    Add kokoro build files

---
+++ kokoro/continuous.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd github/minikube-build-tools-for-java
+
+(cd minikube-gradle-plugin; ./gradlew clean build)
+(cd minikube-maven-plugin; ./mvnw clean install)
+mvn clean install 
