d9391d71ad7111ff317c57621f977a0f0e45cd98
Qingyang Chen <qingyangc@google.com>
2017-12-11 20:26:16 -0500

    working...

--- crepecake/src/main/java/com/google/cloud/tools/crepecake/cache/CacheChecker.java
+++ crepecake/src/main/java/com/google/cloud/tools/crepecake/cache/CacheChecker.java
@@ -4,6 +4,7 @@ import com.google.cloud.tools.crepecake.blob.BlobDescriptor;
 import com.google.cloud.tools.crepecake.image.Image;
 import com.google.cloud.tools.crepecake.image.Layer;
 import com.google.cloud.tools.crepecake.image.LayerException;
+import com.google.cloud.tools.crepecake.image.ReferenceLayer;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,11 +20,11 @@ public class CacheChecker extends CacheHelper {
   /**
    * @return true if the base image is different from the cached base image; false otherwise
    */
-  public boolean isBaseImageModified(Image baseImage) throws IOException {
+  public boolean isBaseImageModified(Image<ReferenceLayer> baseImage) throws IOException {
     CacheMetadata cacheMetadata = getMetadata();
 
     List<TimestampedCachedLayer> cachedLayers = cacheMetadata.getBaseImageLayers();
-    List<Layer> baseImageLayers = baseImage.getLayers();
+    List<ReferenceLayer> baseImageLayers = baseImage.getLayers();
 
     if (cachedLayers.size() != baseImageLayers.size()) {
       return false;
@@ -31,25 +32,78 @@ public class CacheChecker extends CacheHelper {
 
     for (int layerIndex = 0; layerIndex < baseImageLayers.size(); layerIndex ++) {
       BlobDescriptor cachedLayerBlobDescriptor = cachedLayers.get(layerIndex).getBlobDescriptor();
-      BlobDescriptor baseImageLayer.getBlobDescriptor() = baseImageLayers.get(layerIndex);
-      if (cachedLayer.getBlobDescriptor().equals(baseImageLayer.getBlobDescriptor())) {
-
+      BlobDescriptor baseImageBlobDescriptor = baseImageLayers.get(layerIndex).getBlobDescriptor();
+      if (!cachedLayerBlobDescriptor.equals(baseImageBlobDescriptor)) {
+        return false;
       }
     }
+    return true;
   }
 
   /** @return true if the dependencies have been modified since last cache; false otherwise */
-  public boolean isDependenciesLayerModified(BuildOutputDirectories buildOutputDirectories) {
-
+  public boolean isDependenciesLayerModified(BuildOutputDirectories buildOutputDirectories) throws IOException {
+    return isLayerModified(ApplicationLayerType.DEPENDENCIES, buildOutputDirectories);
   }
 
   /** @return true if the resources have been modified since last cache; false otherwise */
-  public boolean isResourcesLayerModified(BuildOutputDirectories buildOutputDirectories) {
-
+  public boolean isResourcesLayerModified(BuildOutputDirectories buildOutputDirectories) throws IOException {
+    return isLayerModified(ApplicationLayerType.RESOURCES, buildOutputDirectories);
   }
 
   /** @return true if the classes have been modified since last cache; false otherwise */
-  public boolean isClassesLayerModified(BuildOutputDirectories buildOutputDirectories) {
+  public boolean isClassesLayerModified(BuildOutputDirectories buildOutputDirectories) throws IOException {
+    return isLayerModified(ApplicationLayerType.CLASSES, buildOutputDirectories);
+  }
 
+  private boolean isLayerModified(ApplicationLayerType layerType, BuildOutputDirectories buildOutputDirectories) throws IOException {
+    CacheMetadata cacheMetadata = getMetadata();
+    TimestampedCachedLayer cachedLayer = cacheMetadata.getApplicationLayer(ApplicationLayerType.CLASSES);
+
+    // Returns true if there is no cached layer.
+    if (null == cachedLayer) {
+      return true;
+    }
+
+    long lastModifiedTime = cachedLayer.getLastModifiedTime();
+
+    switch (layerType) {
+      case DEPENDENCIES:
+        Set<File> filesToCheck = buildOutputDirectories.getDependencies();
+        for (File file : filesToCheck) {
+          if (isFileModifiedRecursive(file, lastModifiedTime)) {
+            return true;
+          }
+        }
+        return false;
+
+      case RESOURCES:
+        return isFileModifiedRecursive(buildOutputDirectories.getResourcesDirectory(), lastModifiedTime);
+
+      case CLASSES:
+        return isFileModifiedRecursive(buildOutputDirectories.getClassesDirectory(), lastModifiedTime);
+    }
+  }
+
+  /**
+   * Checks the file has been modified since the {@code lastModifiedTime}. Recursively checks all subfiles if {@code file} is a directory.
+   */
+  private boolean isFileModifiedRecursive(File file, long lastModifiedTime) throws IOException {
+    if (file.lastModified() > lastModifiedTime) {
+      return true;
+    }
+
+    if (file.isDirectory()) {
+      File[] subFiles = file.listFiles();
+      if (null == subFiles) {
+        throw new IOException("Failed to read directory: " + file.getAbsolutePath());
+      }
+      for (final File subFile : subFiles) {
+        if (isFileModifiedRecursive(subFile, lastModifiedTime)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
