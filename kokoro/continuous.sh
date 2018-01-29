c08fdec6ab2d2811f05c08a0760f298820a9847d
Qingyang Chen <qingyangc@google.com>
2018-01-28 19:19:38 -0500

    Removes crepecake.

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -7,4 +7,3 @@ cd github/minikube-build-tools-for-java
 
 (cd minikube-gradle-plugin; ./gradlew clean build)
 (cd minikube-maven-plugin; ./mvnw clean install)
-(cd crepecake; ./gradlew clean build --info)
