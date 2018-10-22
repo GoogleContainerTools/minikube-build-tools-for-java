3ba3b4f5466b97c7cf5bb6d70c86157eef1f1dae
Gintautas Sulskus <gintautas.sulskus@gmail.com>
2018-10-22 15:53:48 +0100

    applied google code styling

--- minikube-gradle-plugin/src/main/java/com/google/cloud/tools/minikube/MinikubeExtension.java
+++ minikube-gradle-plugin/src/main/java/com/google/cloud/tools/minikube/MinikubeExtension.java
@@ -18,69 +18,66 @@ package com.google.cloud.tools.minikube;
 
 import com.google.cloud.tools.minikube.util.CommandExecutorFactory;
 import com.google.cloud.tools.minikube.util.MinikubeDockerEnvParser;
-import org.gradle.api.Project;
-import org.gradle.api.provider.PropertyState;
-import org.gradle.internal.impldep.com.google.common.base.Preconditions;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import org.gradle.api.Project;
+import org.gradle.api.provider.PropertyState;
+import org.gradle.internal.impldep.com.google.common.base.Preconditions;
 
-/**
- * Minikube configuration extension.
- */
+/** Minikube configuration extension. */
 public class MinikubeExtension {
 
-    private final PropertyState<String> minikube;
+  private final PropertyState<String> minikube;
 
-    private final CommandExecutorFactory commandExecutorFactory;
+  private final CommandExecutorFactory commandExecutorFactory;
 
-    public MinikubeExtension(Project project, CommandExecutorFactory commandExecutorFactory) {
-        minikube = project.property(String.class);
-        setMinikube("minikube");
+  public MinikubeExtension(Project project, CommandExecutorFactory commandExecutorFactory) {
+    minikube = project.property(String.class);
+    setMinikube("minikube");
 
-        this.commandExecutorFactory = commandExecutorFactory;
-    }
+    this.commandExecutorFactory = commandExecutorFactory;
+  }
 
-    public String getMinikube() {
-        return minikube.get();
-    }
+  public String getMinikube() {
+    return minikube.get();
+  }
 
-    public void setMinikube(String minikube) {
-        this.minikube.set(minikube);
-    }
+  public void setMinikube(String minikube) {
+    this.minikube.set(minikube);
+  }
 
-    public PropertyState<String> getMinikubeProvider() {
-        return minikube;
-    }
+  public PropertyState<String> getMinikubeProvider() {
+    return minikube;
+  }
 
-    /**
-     * Gets the minikube docker environment variables by running the command 'minikube docker-env
-     * --shell=none'.
-     *
-     * @return A map of docker environment variables and their values
-     */
-    public Map<String, String> getDockerEnv() throws IOException, InterruptedException {
-        return getDockerEnv("");
-    }
+  /**
+   * Gets the minikube docker environment variables by running the command 'minikube docker-env
+   * --shell=none'.
+   *
+   * @return A map of docker environment variables and their values
+   */
+  public Map<String, String> getDockerEnv() throws IOException, InterruptedException {
+    return getDockerEnv("");
+  }
 
-    /**
-     * Gets the minikube docker environment variables by running the command 'minikube docker-env
-     * --shell=none'.
-     *
-     * @param profile Target minikube profile
-     * @return A map of docker environment variables and their values
-     */
-    public Map<String, String> getDockerEnv(String profile) throws IOException, InterruptedException {
-        Preconditions.checkNotNull(profile, "Minikube profile must not be null");
+  /**
+   * Gets the minikube docker environment variables by running the command 'minikube docker-env
+   * --shell=none'.
+   *
+   * @param profile Target minikube profile
+   * @return A map of docker environment variables and their values
+   */
+  public Map<String, String> getDockerEnv(String profile) throws IOException, InterruptedException {
+    Preconditions.checkNotNull(profile, "Minikube profile must not be null");
 
-        List<String> minikubeDockerEnvCommand =
-                Arrays.asList(minikube.get(), "docker-env", "--shell=none", "--profile=".concat(profile));
+    List<String> minikubeDockerEnvCommand =
+        Arrays.asList(minikube.get(), "docker-env", "--shell=none", "--profile=".concat(profile));
 
-        List<String> dockerEnv =
-                commandExecutorFactory.newCommandExecutor().run(minikubeDockerEnvCommand);
+    List<String> dockerEnv =
+        commandExecutorFactory.newCommandExecutor().run(minikubeDockerEnvCommand);
 
-        return MinikubeDockerEnvParser.parse(dockerEnv);
-    }
+    return MinikubeDockerEnvParser.parse(dockerEnv);
+  }
 }
--- minikube-gradle-plugin/src/test/java/com/google/cloud/tools/minikube/MinikubeExtensionTest.java
+++ minikube-gradle-plugin/src/test/java/com/google/cloud/tools/minikube/MinikubeExtensionTest.java
@@ -16,99 +16,98 @@
 
 package com.google.cloud.tools.minikube;
 
+import static org.mockito.Mockito.*;
+
 import com.google.cloud.tools.minikube.util.CommandExecutor;
 import com.google.cloud.tools.minikube.util.CommandExecutorFactory;
+import java.io.IOException;
+import java.util.*;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.*;
-
-import static org.mockito.Mockito.*;
-
-/**
- * Tests for MinikubeExtension
- */
+/** Tests for MinikubeExtension */
 public class MinikubeExtensionTest {
 
-    private CommandExecutor commandExecutorMock;
-    private CommandExecutorFactory commandExecutorFactoryMock;
-    private MinikubeExtension minikube;
+  private CommandExecutor commandExecutorMock;
+  private CommandExecutorFactory commandExecutorFactoryMock;
+  private MinikubeExtension minikube;
 
-    List<String> expectedCommand;
-    private static final List<String> dockerEnvOutput = Arrays.asList("ENV_VAR1=VAL1", "ENV_VAR2=VAL2");
-    private static final Map<String, String> expectedMap;
+  List<String> expectedCommand;
+  private static final List<String> dockerEnvOutput =
+      Arrays.asList("ENV_VAR1=VAL1", "ENV_VAR2=VAL2");
+  private static final Map<String, String> expectedMap;
 
-    static {
-        Map<String, String> map = new HashMap<>(2);
-        map.put("ENV_VAR1", "VAL1");
-        map.put("ENV_VAR2", "VAL2");
-        expectedMap = Collections.unmodifiableMap(map);
-    }
+  static {
+    Map<String, String> map = new HashMap<>(2);
+    map.put("ENV_VAR1", "VAL1");
+    map.put("ENV_VAR2", "VAL2");
+    expectedMap = Collections.unmodifiableMap(map);
+  }
 
-    /*
-     * Reinitialise variables before each test.
-     */
-    @Before
-    public void setUp() {
-        Project project = ProjectBuilder.builder().build();
+  /*
+   * Reinitialise variables before each test.
+   */
+  @Before
+  public void setUp() {
+    Project project = ProjectBuilder.builder().build();
 
-        // Mocks the CommandExecutor.
-        commandExecutorMock = mock(CommandExecutor.class);
-        commandExecutorFactoryMock = mock(CommandExecutorFactory.class);
-        when(commandExecutorFactoryMock.newCommandExecutor()).thenReturn(commandExecutorMock);
+    // Mocks the CommandExecutor.
+    commandExecutorMock = mock(CommandExecutor.class);
+    commandExecutorFactoryMock = mock(CommandExecutorFactory.class);
+    when(commandExecutorFactoryMock.newCommandExecutor()).thenReturn(commandExecutorMock);
 
-        // Creates an extension to test on.
-        minikube = new MinikubeExtension(project, commandExecutorFactoryMock);
-        minikube.setMinikube("/test/path/to/minikube");
+    // Creates an extension to test on.
+    minikube = new MinikubeExtension(project, commandExecutorFactoryMock);
+    minikube.setMinikube("/test/path/to/minikube");
 
-        expectedCommand = new LinkedList<>(Arrays.asList("/test/path/to/minikube", "docker-env", "--shell=none"));
-    }
+    expectedCommand =
+        new LinkedList<>(Arrays.asList("/test/path/to/minikube", "docker-env", "--shell=none"));
+  }
 
-    /*
-     * Test with default minikube profile
-     */
-    @Test
-    public void testGetDockerEnvWithDefaultProfile() throws IOException, InterruptedException {
-        expectedCommand.add("--profile=");
-        when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
-        Assert.assertEquals(expectedMap, minikube.getDockerEnv());
-        verify(commandExecutorMock).run(expectedCommand);
-    }
+  /*
+   * Test with default minikube profile
+   */
+  @Test
+  public void testGetDockerEnvWithDefaultProfile() throws IOException, InterruptedException {
+    expectedCommand.add("--profile=");
+    when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
+    Assert.assertEquals(expectedMap, minikube.getDockerEnv());
+    verify(commandExecutorMock).run(expectedCommand);
+  }
 
-    /*
-     * Test with 'testProfile'
-     */
-    @Test
-    public void testGetDockerEnvWithTestProfile() throws IOException, InterruptedException {
-        String profile = "testProfile";
-        expectedCommand.add("--profile=".concat(profile));
-        when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
-        Assert.assertEquals(expectedMap, minikube.getDockerEnv(profile));
-        verify(commandExecutorMock).run(expectedCommand);
-    }
+  /*
+   * Test with 'testProfile'
+   */
+  @Test
+  public void testGetDockerEnvWithTestProfile() throws IOException, InterruptedException {
+    String profile = "testProfile";
+    expectedCommand.add("--profile=".concat(profile));
+    when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
+    Assert.assertEquals(expectedMap, minikube.getDockerEnv(profile));
+    verify(commandExecutorMock).run(expectedCommand);
+  }
 
-    /*
-     * Make sure both minikube.getDockerEnv() and minikube.getDockerEnv("") refer to the default minikube profile
-     */
-    @Test
-    public void testGetSameDockerEnvWithTwoDefaultProfiles() throws IOException, InterruptedException {
-        String profile = "";
-        expectedCommand.add("--profile=".concat(profile));
-        when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
-        Assert.assertEquals(minikube.getDockerEnv(), minikube.getDockerEnv(profile));
-        verify(commandExecutorMock, times(2)).run(expectedCommand);
-    }
+  /*
+   * Make sure both minikube.getDockerEnv() and minikube.getDockerEnv("") refer to the default minikube profile
+   */
+  @Test
+  public void testGetSameDockerEnvWithTwoDefaultProfiles()
+      throws IOException, InterruptedException {
+    String profile = "";
+    expectedCommand.add("--profile=".concat(profile));
+    when(commandExecutorMock.run(expectedCommand)).thenReturn(dockerEnvOutput);
+    Assert.assertEquals(minikube.getDockerEnv(), minikube.getDockerEnv(profile));
+    verify(commandExecutorMock, times(2)).run(expectedCommand);
+  }
 
-    /*
-     * getDockerEnv() should not permit null values
-     */
-    @Test(expected = NullPointerException.class)
-    public void testGetDockerEnvWithNullProfile() throws IOException, InterruptedException {
-        minikube.getDockerEnv(null);
-    }
-
+  /*
+   * getDockerEnv() should not permit null values
+   */
+  @Test(expected = NullPointerException.class)
+  public void testGetDockerEnvWithNullProfile() throws IOException, InterruptedException {
+    minikube.getDockerEnv(null);
+  }
 }
