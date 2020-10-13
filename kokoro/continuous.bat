b4cf6fd36abc3f20d7eeac3990a97c7eb2db8836
Chanseok Oh <chanseok@google.com>
2020-10-13 10:16:06 -0400

    Update continuous.bat

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -6,6 +6,6 @@ rem there is no minikube installed on this system, so any integration test will 
 rem we porbably also have to install VirtualBox or some other VM driver, it not even clear to me
 rem that kokoro will allow us to run a VM.
 
-cd minikube-gradle-plugin && call gradlew.bat clean build && ^
-cd ../minikube-maven-plugin && call mvnw.bat clean install
+cd minikube-gradle-plugin && call gradlew.bat --console=plain clean build && ^
+cd ../minikube-maven-plugin && call mvnw.bat -B -U clean verify
 exit /b %ERRORLEVEL%
